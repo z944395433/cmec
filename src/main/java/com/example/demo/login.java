41b7d6392045ccd409e8547220fdcf23ac440539
z944395433 <944395433@qq.com>
2018-09-06 23:35:51 +0800

    创建

---
+++ src/main/java/com/example/demo/login.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/sys")
+public class login {
+
+    @RequestMapping("/login.php")
+    public String login () {
+        System.out.println("hello world");
+        return "hello world";
+
+    }
+}
